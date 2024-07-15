f6b8825836727480b2dba12578003eb7ae6dfa18
jsween5723 <jsween5723@gmail.com>
2024-07-15 15:16:40 +0900

    feat: 반복문 구현

--- src/main/java/com/example/demo/api/route/TestController.java
+++ src/main/java/com/example/demo/api/route/TestController.java
@@ -1,6 +1,9 @@
 package com.example.demo.api.route;
 
+import com.example.demo.domain.Product;
 import com.example.demo.domain.User;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +15,11 @@ public class TestController {
   public String test(Model model) {
     User user = new User("jsween5723", "jsween5723@gmail.com", false);
     model.addAttribute("user", user);
-
+    ArrayList<Product> products = new ArrayList<>();
+    products.add(new Product(1L, "샘플1", 10));
+    products.add(new Product(2L, "샘플2", 20));
+    products.add(new Product(3L, "샘플3", 30));
+    model.addAttribute("products", products);
     return "index";
   }
 }
---
+++ src/main/java/com/example/demo/domain/Product.java
@@ -0,0 +1,5 @@
+package com.example.demo.domain;
+
+public record Product(long id, String name, int price) {
+
+}
