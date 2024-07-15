fbd01a12fb81aba5c29db7b1b323a20fb5b41ee9
jsween5723 <jsween5723@gmail.com>
2024-07-15 15:59:21 +0900

    refactor: 불필요 import 제거

--- src/main/java/com/example/demo/route/BookController.java
+++ src/main/java/com/example/demo/route/BookController.java
@@ -1,15 +1,10 @@
 package com.example.demo.route;
 
-import com.example.demo.domain.Book;
 import com.example.demo.domain.BookService;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("book")
