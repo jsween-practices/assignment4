b99669f6bcf5ce23a5355952d6669af62fb45942
jsween5723 <jsween5723@gmail.com>
2024-07-15 14:53:35 +0900

    project init 및 기본 설정 및 환경 구성

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+  @GetMapping("index")
+  public String test(Model model) {
+    return "index";
+  }
+}
