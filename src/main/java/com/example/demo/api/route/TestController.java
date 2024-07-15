a8a1700257c5f8fddc7a4def733da9071bddbcbd
jsween5723 <jsween5723@gmail.com>
2024-07-15 15:09:28 +0900

    feat: 변수 표현식 사용

--- src/main/java/com/example/demo/api/route/TestController.java
+++ src/main/java/com/example/demo/api/route/TestController.java
@@ -1,13 +1,18 @@
 package com.example.demo.api.route;
 
+import com.example.demo.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+
   @GetMapping("index")
   public String test(Model model) {
+    User user = new User("jsween5723", "jsween5723@gmail.com", false);
+    model.addAttribute("user", user);
+
     return "index";
   }
 }
---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,5 @@
+package com.example.demo.domain;
+
+public record User(String username, String email, boolean isAdmin) {
+
+}
